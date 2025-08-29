2ee42226fb9988effda9350ae5e409bb6ff9bed2
otto-mata <ottomata@proton.me>
2025-08-29 16:02:08 +0200

    feat(core): Channels
    refacto(IRCServer): #2 - use map for users
    
    fix(IRCServer): remove old vector
    
    feat(core): Channels
    
    fix rebase mistake
    
    chore(IRCServer): minor cleanup
    
    fix(logger): wrong proto
    
    feat(IRCServer) use logger
    
    fix je suis nul ptn
    
    fix je suis nul ptn bis
    
    build(core+logger): fixed errors and added loggerlib to comp

--- IRCServer.cpp
+++ IRCServer.cpp
@@ -2,21 +2,10 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-struct client_by_fd
-{
-public:
-  client_by_fd(int cfd)
-    : fd(cfd)
-  {
-  }
-  bool operator()(IRCClient const& c) { return c.fd() == fd; }
-
-private:
-  int fd;
-};
 
 IRCServer::IRCServer()
   : SocketServer(6667)
+  , logger("IRCServer", Logging::DEBUG)
 {
 }
 
@@ -32,11 +21,11 @@ IRCServer::onRequest(Request* req)
   IRCClient* user;
 
   if (itClient != cMap.end() && req->size() == 0) {
-    std::cout << "[*][IRCServer] Disconnected client" << std::endl;
+    logger.debug("Client disconnected");
     cMap.erase(itClient);
     return 0;
   } else if (itClient == cMap.end()) {
-    std::cout << "[+][IRCServer] New client" << std::endl;
+    logger.log("New client");
     Client& c = clientByFileno(req->origin());
     cMap[req->origin()] = new IRCClient(c);
   }
@@ -58,5 +47,44 @@ IRCServer::onClientDisconnect(Client& c)
   std::map<int, IRCClient*>::iterator itClient = cMap.find(c.fileno());
 
   cMap.erase(itClient);
-  std::cout << "[*][SocketServer] Client disconnected." << std::endl;
+  logger.debug("Client disconnected");
+}
+
+void
+IRCServer::createChannel(std::string channelName)
+{
+  if (_channels.find(channelName) != _channels.end()) {
+    //! TODO: ERROR HANDLING
+
+    logger.err("Cannot create channel '" + channelName +
+               "'. Channel already exists.");
+    return;
+  }
+  _channels[channelName] = IRCChannel(channelName);
+}
+
+void
+IRCServer::addUserToChannel(IRCClient* c, std::string channelName)
+{
+  if (_channels.find(channelName) == _channels.end()) {
+    //! TODO: ERROR HANDLING
+
+    logger.err("Cannot add user to channel '" + channelName +
+               "'. Channel doesn't exist.");
+    return;
+  }
+  _channels[channelName].addUser(c);
+}
+
+void
+IRCServer::addAdminToChannel(IRCClient* c, std::string channelName)
+{
+  if (_channels.find(channelName) == _channels.end()) {
+    //! TODO: ERROR HANDLING
+
+    logger.err("Cannot add admin to channel '" + channelName +
+               "'. Channel doesn't exist.");
+    return;
+  }
+  _channels[channelName].adminUser(c);
 }
