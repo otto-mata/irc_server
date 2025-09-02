597876d097e5f0840b06437bc9088111bf2fbd80
otto-mata <ottomata@proton.me>
2025-09-02 14:12:14 +0200

    update channel class to be in accordance with #7

--- IRCChannel.hpp
+++ IRCChannel.hpp
@@ -1,62 +1,73 @@
 #ifndef IRCCHANNEL_HPP
-#define IRCCHANNEL_HPP
+# define IRCCHANNEL_HPP
 
 #include "IRCClient.hpp"
-#include <iostream>
-#include <map>
-#include <set>
-#include <string>
+# include <iostream>
+# include <string>
+# include <map>
+# include <set>
+
+#define DEFAULT_NAME "unamed_channel"
+#define DEFAULT_TOPIC "undefined_topic"
+#define DEFAULT_USER_LIMIT_COUNT 42
+#define DEFAULT_INVITE_ONLY 0
+#define DEFAULT__TOPIC_MODIFIABLE 1
+#define DEFAULT__USER_LIMIT 0
 
 class IRCChannel
 {
-private:
-  std::string _name;
-  std::set<IRCClient*> _users;
-  IRCClient* _owner;
-  std::string _password;
-  size_t _maxUsers;
-  bool _inviteOnly;
-  bool _protectedTopic;
-  std::string _topic;
-  bool _botChannelActive;
-  std::set<IRCClient*> _admins;
-  std::set<IRCClient*> _guests;
+	private:
+		std::string _name;
+		std::set<IRCClient*> _users;
+		std::set<IRCClient*> _admins;
+		std::string _topic;
+		int _userLimitCount;
+		bool _inviteOnly;
+		bool _topicModifiable;
+		bool _userLimit;
 
-public:
-  IRCChannel();
-  IRCChannel(std::string channel_name);
-  IRCChannel(const IRCChannel& other);
-  IRCChannel& operator=(const IRCChannel& other);
-  ~IRCChannel();
-  std::string getTopic() const;
-  size_t getMaxUsers() const;
-  IRCClient getOwner() const;
-  std::set<IRCClient*> getGuests() const;
-  std::string getPassword() const;
-  bool isUserInChannel(IRCClient* user) const;
-  bool isInviteOnly() const;
-  bool isProtectedTopic() const;
+	public:
+		IRCChannel();
+		IRCChannel(std::string channel_name);
+		IRCChannel(const IRCChannel& other);
+		IRCChannel& operator=(const IRCChannel& other);
+		~IRCChannel();
 
-  std::string getName();
-  std::set<IRCClient*> getUserList();
-  IRCClient* getUser(std::string user_name);
+		std::string getName();
+		std::set<IRCClient*> getUserList();
+		IRCClient* getUser(std::string user_name);
 
-  std::set<IRCClient*> getAdminList();
-  IRCClient* getAdmin(std::string admin_name);
+		std::set<IRCClient*> getAdminList();
+		IRCClient* getAdmin(std::string admin_name);
 
-  bool isUser(IRCClient* user_tofind);
-  bool isUser(int user_tofind);
-  bool isUser(std::string user_tofind);
+		std::string getTopic();
+		int getUserLimitCount();
+		bool getInviteOnly();
+		bool getTopicModifiable();
+		bool getUserLimit();
 
-  bool isAdmin(IRCClient* admin_tofind);
-  bool isAdmin(int admin_tofind);
-  bool isAdmin(std::string admin_tofind);
+		void setTopic(std::string NewTopic);
+		void setUserLimitCount(int NewUserLimit);
+		void setInviteOnly(bool info);
+		void setTopicModifiable(bool info);
+		void setUserLimit(bool info);
 
-  void addUser(IRCClient* user_toadd);
-  void removeUser(IRCClient* user_toremove);
+		bool isUser(IRCClient* user_tofind);
+		bool isUser(int user_tofind);
+		bool isUser(std::string user_tofind);
 
-  void adminUser(IRCClient* admin_toadd);
-  void removeadminUser(IRCClient* admin_toremove);
+		bool isAdmin(IRCClient* admin_tofind);
+		bool isAdmin(int admin_tofind);
+		bool isAdmin(std::string admin_tofind);
+
+		void addUser(IRCClient* user_toadd);
+		void removeUser(IRCClient* user_toremove);
+
+		void adminUser(IRCClient* admin_toadd);
+		void removeadminUser(IRCClient* admin_toremove);
+
+
+	
 };
 
 #endif
